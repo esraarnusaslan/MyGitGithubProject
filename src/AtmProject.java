053fe8ef2d5fc37a6f2419acc84bbd4ba599ba65
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-04-27 09:43:42 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir Atm projesidir");
         //version 1
+        System.out.println("Java is easy");
+        //version 2
     }
 }
